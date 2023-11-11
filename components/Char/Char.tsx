655b24db19f0107f237a701d6f2506bdf8903acb
NormalPlayerJSH <jsh010904@gmail.com>
2023-11-11 22:57:04 +0900

    캐릭터 커마 완성

--- components/Char/Char.tsx
+++ components/Char/Char.tsx
@@ -12,6 +12,22 @@ export function Char() {
           <div className="w-full aspect-[3/4] relative">
             <Image src="/fullBefore.png" alt="캐릭터" objectFit="contain" fill />
           </div>
+          <div className="w-full py-3 flex items-center justify-center">
+            <div className="join">
+              <button type="button" className="btn btn-primary join-item">상의</button>
+              <button type="button" className="btn join-item">하의</button>
+              <button type="button" className="btn join-item">신발</button>
+              <button type="button" className="btn join-item">액세서리</button>
+            </div>
+          </div>
+          <div className="w-full p-3 flex flex-row justify-around">
+            <div className="w-[35%] aspect-square relative border-4 border-primary rounded-lg overflow-hidden">
+              <Image src="/wear1.png" alt="캐릭터" objectFit="contain" fill />
+            </div>
+            <div className="w-[35%] aspect-square relative border-4 border-gray-300 rounded-lg overflow-hidden">
+              <Image src="/wear2.png" alt="캐릭터" objectFit="contain" fill />
+            </div>
+          </div>
         </div>
       </div>
     </Center>
